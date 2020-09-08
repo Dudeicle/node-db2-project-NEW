01768a0f5fc4dc2cd0bccd8776252881f2479256
Dudeicle <brian-griffiths@lambdastudents.com>
2020-09-08 12:48:22 -0400

    most of setup done

---
+++ migrations/20200908123702_v1.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+	return knex.schema.createTable("cars", (tbl) => {
+		// PK
+		tbl.increments("id");
+
+		// VIN number, required field
+		tbl.integer("vin").notNullable();
+
+		// Make, required field
+		tbl.string("make").notNullable();
+
+		// Model, required field
+		tbl.string("model").notNullable();
+
+		// Mileage, required field
+		tbl.integer("mileage").notNullable();
+
+		// Transmission, non-required
+		tbl.string("transmission");
+
+		// Status of Title, non-required
+		tbl.string("title");
+	});
+};
+
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists("cars");
+};
